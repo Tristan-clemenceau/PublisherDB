eba3fd845f4e2c2f0a64ba4dafeb40c1ab7bd809
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-03-20 17:42:58 +0000

    adding getter and setter for Book class

--- ProjectDb/Book.cpp
+++ ProjectDb/Book.cpp
@@ -1,12 +1,42 @@
 #include "ProjectDbHeader.h"
 using namespace std;
 
+/*CONSTRUCTOR*/
 Book::Book(){
     Book::id = "Unset";
     Book::title = "Unset";
     Book::nbPage= 0;
     Book::bookContent ="Unset";
 }
+/*DESTRUCTOR*/
+/*GETTER*/
+string Book::getId(){
+	return Book::id;
+}
+string Book::getTitle(){
+    return Book::title;
+}
+int Book::getNbPage(){
+    return Book::nbPage;
+}
+string Book::getBookContent(){
+    return Book::bookContent;
+}
+/*SETTER*/
+void Book::setId(string Book_id){
+    Book::id = Book_id;
+}
+void Book::setTitle(string Book_title){
+    Book::title = Book_title;
+}
+void Book::setNbPage(int Book_nbPage){
+    Book::nbPage = Book_nbPage;
+}
+void Book::setBookContent(string Book_bookContent){
+    Book::bookContent = Book_bookContent;
+}
+/*OTHER*/
+/*OBJECT METHOD*/
 ostream& operator<<(ostream &out,const Book &b){
     out<<"[Book]\t Title : "<<b.title<<"\t Id : "<<b.id<<endl;
     return out;
