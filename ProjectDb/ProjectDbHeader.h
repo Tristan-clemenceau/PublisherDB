bd5e38d97aea2766e7f3c3ef5a159cd587bcadf3
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-03-20 16:16:21 +0000

    Adding the first declaration of the classes

--- ProjectDb/ProjectDbHeader.h
+++ ProjectDb/ProjectDbHeader.h
@@ -1,6 +1,36 @@
 #ifndef PROJECTDBHEADER_H_INCLUDED
 #define PROJECTDBHEADER_H_INCLUDED
 
+using namespace std;
 
+class Publisher{
+};
+
+class Book{
+};
+
+class PaperBook{
+};
+
+class HardBack{
+};
+
+class Magazines{
+};
+
+class Advertisement{
+};
+
+class Advertiser{
+};
+
+class Company{
+};
+
+class PhysicalPerson{
+};
+
+class Person{
+};
 
 #endif // PROJECTDBHEADER_H_INCLUDED
